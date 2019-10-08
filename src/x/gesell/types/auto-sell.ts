a54af001f959da8f80a42deb20fe437e27fcbafb
unknown <33382781+YuKimura45z@users.noreply.github.com>
2019-10-08 14:14:56 +0900

    [add]types

---
+++ src/x/gesell/types/auto-sell.ts
@@ -0,0 +1,7 @@
+export interface AutoSell {
+  price1000: number;
+  iso4217: string;
+  real_name: string;
+  real_address: string;
+  bank_account: string;
+}
---
+++ src/x/gesell/types/expiration.ts
@@ -0,0 +1,5 @@
+export interface Expiration {
+  year: number;
+  month: number;
+  day: number;
+}
