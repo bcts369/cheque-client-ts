ffd52729f77a3d9e93523f07a52867d30fbfc942
bcts369 <41810380+bcts369@users.noreply.github.com>
2019-11-17 20:06:45 +0800

    add BuyTokenReq interface

---
+++ src/x/gesell/types/buy-token-req.ts
@@ -0,0 +1,10 @@
+import { BaseReq } from "cosmos-client-ts/lib/types/cosmos-sdk/rest";
+import { Expiration } from "./expiration";
+
+
+export interface BuyTokenReq {
+    base_req: BaseReq; // cosmos-client-ts
+    address: string;
+    expiration: Expiration;
+    amount: number;
+}
